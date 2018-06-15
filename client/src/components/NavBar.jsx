7c7f0e5ee804d1d5285d02a90497f5b0591a1ba0
romcar <rommel.carrasquilla@gmail.com>
2018-06-14 22:33:19 -0400

    Include SearchEnhancer component
    - Refactored render function to display SearchEnhancer
      when the user searches for something.
    - Made sure that the SearchEnhancer hides when the user
      is not searching for anything.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,12 +3,18 @@ import Signup from './Signup.jsx';
 import Login from './Login.jsx';
 import NavDropdown from './NavDropdown.jsx';
 import {Button, Dropdown, Menu, Container, Header, Icon} from 'semantic-ui-react';
+import SearchEnhancer from './SearchEnhancer.jsx';
 
 class NavBar extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      search: ''
+    }
   }
-
+  renderSearchEnhancer() {
+    return(<SearchEnhancer query={this.state.search}>{this.state.search}</SearchEnhancer>)
+  }
   renderCredential(){
     if(this.props.session === null){
       return(
@@ -32,33 +38,36 @@ class NavBar extends React.Component{
 
   render(){
     return(
-      <div className="ui menu aligned">
-        <div className='item'>
-            <Icon onClick={this.props.homeHandler.bind(this)} name='home' size='large'></Icon>
-        </div>
-        <div className="item">
-          <div className="ui action left icon input">
-          {/*search bar here*/}
-            <form onSubmit={this.handleSearch.bind(this)}>
-              <i className="search icon"></i>
-              <input className="search-query"type="type" placeholder="Search"/>
-              <Button color='orange' className="ui button">Submit</Button>
-            </form>
+      <div>
+        <div className="ui menu aligned">
+          <div className='item'>
+              <Icon onClick={this.props.homeHandler.bind(this)} name='home' size='large'></Icon>
           </div>
+          <div className="item">
+            <div className="ui action left icon input">
+            {/*search bar here*/}
+              <form onSubmit={this.handleSearch.bind(this)}>
+                <i className="search icon"></i>
+                <input className="search-query"type="type" placeholder="Search"/>
+                <Button color='orange' className="ui button">Submit</Button>
+              </form>
+            </div>
+          </div>
+          <Menu.Menu position="right">
+              {this.renderCredential()}
+            <NavDropdown
+            listings={this.props.listings}
+            logout={this.props.logout.bind(this)}
+            session={this.props.session}
+            createListing={this.props.createListing.bind(this)}
+            delete={this.props.delete.bind(this)}
+            listingSelectHandler={this.props.listingSelectHandler.bind(this)}
+            logout={this.props.logout.bind(this)}
+            giveHandler={this.props.giveHandler.bind(this)}>
+            </NavDropdown>
+          </Menu.Menu>
         </div>
-        <Menu.Menu position="right">
-            {this.renderCredential()}
-          <NavDropdown
-          listings={this.props.listings}
-          logout={this.props.logout.bind(this)}
-          session={this.props.session}
-          createListing={this.props.createListing.bind(this)}
-          delete={this.props.delete.bind(this)}
-          listingSelectHandler={this.props.listingSelectHandler.bind(this)}
-          logout={this.props.logout.bind(this)}
-          giveHandler={this.props.giveHandler.bind(this)}>
-          </NavDropdown>
-        </Menu.Menu>
+        {this.state.search ? this.renderSearchEnhancer() : undefined}
       </div>
     )
   }
@@ -66,9 +75,10 @@ class NavBar extends React.Component{
   handleSearch(e) {
     e.preventDefault();
     const query = document.getElementsByClassName('search-query')[0].value;
-
+    this.setState({search: query});
+    document.getElementsByClassName('search-query')[0].value = '';
     this.props.searchListings(query);
-    console.log(query)
+
   }
 }
 
