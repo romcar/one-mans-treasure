ecec043da9f6b0adb9b75b1044b9f7867acff373
Heshie London <heshielondon@gmail.com>
2018-06-15 14:46:36 -0700

    init for FETCH_USER redux action

--- client/src/reducers/UsersReducer.js
+++ client/src/reducers/UsersReducer.js
@@ -1,4 +1,6 @@
 import { FETCH_INTERESTED_USERS } from "../actions/UserActions";
+// import { FETCH_USER } from "../actions/UserActions";
+
 
 export default (state = [], action) =>{
   switch(action.type){
@@ -6,4 +8,11 @@ export default (state = [], action) =>{
   }
   return state;
 }
+
+// module.exports.fetchUser = function(state = [], action) {
+//   switch(action.type){
+//     case FETCH_USER: return action.payload.data;
+//   }
+//   return state;
+// }
 
