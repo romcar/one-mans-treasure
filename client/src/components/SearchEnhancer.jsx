ba17ac09d767d2219537fc6c1778a16d534a95c2
romcar <rommel.carrasquilla@gmail.com>
2018-06-15 16:50:24 -0400

    Remove relevance default - implement sorting algorithms for:   - Titles   - Highest Claims   - Lowest Claims

--- client/src/components/SearchEnhancer.jsx
+++ client/src/components/SearchEnhancer.jsx
@@ -1,33 +1,84 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Button } from 'semantic-ui-react';
+import store from '../index.jsx';
 
 class SearchEnhancer extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      sortType: 'relavance'
+      sortType: 'default'
     }
   }
 
   handleSortChange(e, {name}) {
+    // debugger;
+    if(name !== 'default') {
+      this.sortBy(name);
+    }
     this.setState({sortType: name});
   }
 
+  sortBy(condition) {
+    let listings = store.getState().listings.listings;
+
+    if(condition === 'claimsDesc' || condition === 'claimsAsc') {
+      let key = 'interested_users'
+
+      listings = this.compare(listings, key, condition);
+
+      store.dispatch({
+        type: 'SET_LISTINGS',
+        payload: listings
+      });
+    } else if(condition === 'name') {
+      let key = 'title'
+
+      listings = this.compare(listings, key, condition);
+
+      store.dispatch({
+        type: 'SET_LISTINGS',
+        payload: listings
+      });
+    }
+  }
+
+  compare(listings, key, sortType) {
+    if(sortType === 'claimsDesc') {
+      return listings.sort((a,b) => {
+        if(a[key].length < b[key].length) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+    } else if(sortType === 'claimsAsc') {
+      return listings.sort((a,b) => {
+        if(a[key].length < b[key].length) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+    } else if(sortType === 'name') {
+      return listings.sort((a,b) => {
+        if(a[key] < b[key]) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+    }
+  }
   render() {
     const {sortType} = this.state;
-
+    const color = "orange";
     return(
       <div>
         <Menu>
 
-          <div>Searching for: {this.props.query}</div>
-
-          <Menu.Item
-            name='relevance'
-            active={sortType === 'relevance'}
-            onClick={this.handleSortChange.bind(this)}>
-            Relevance
-          </Menu.Item>
+            <Button color='orange'>
+              {`Searching for: ${this.props.query}`}
+            </Button>
 
           <Menu.Item
             name='claimsDesc'
