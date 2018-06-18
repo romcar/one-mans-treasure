bf1d1903355c3ae82af44f7c912a8a6c8ebae937
Heshie London <30602330+H-London@users.noreply.github.com>
2018-06-18 10:25:56 -0700

    Remove console.log statements

--- server/controllers/comment-controller.js
+++ server/controllers/comment-controller.js
@@ -4,21 +4,16 @@ const util = require('../services/utility.js');
 module.exports = {
   comments: {
     post: function(req, res) {
-      console.log('This is the request body: ', req.body)
       db.saveComment(req.body).then(savedComment => {
-        console.log('Saved comment: ', savedComment)
         res.status(201).send(savedComment);
       }).catch(error => {
-        console.log('error: ', error)
         res.status(404).send(error);
       });
     },
     get: function(req, res) {
       db.findCommentsList().then(commentsList => {
-        console.log('This is my comments list: ', commentsList)
         res.status(201).send(commentsList)
       }).catch(error => {
-        console.log('error in getting comments list: ', error)
         res.status(404).send(error)
       });
     }
