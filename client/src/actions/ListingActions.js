cfaad41aeaa788b3ae1bdf15ce920a1c62c60b7f
JohnWebb4 <John.Webb94@gmail.com>
2018-06-18 15:39:22 -0700

    Lint front-end files

--- client/src/actions/ListingActions.js
+++ client/src/actions/ListingActions.js
@@ -6,42 +6,42 @@ export const SET_QUERY = 'SET_QUERY';
 export const SET_LISTINGS = 'SET_LISTINGS';
 
 export const fetchListings = (query) =>{
-  if(query) {
+  if (query) {
     const url = `/listing?query=${query}`;
     const response = axios.get(url);
     return {
       type: FETCH_LISTINGS,
       payload: response
-    }
+    };
   } else {
-    const url = `/listing`;
+    const url = '/listing';
     const response = axios.get(url);
     return {
       type: FETCH_LISTINGS,
       payload: response
-    }
+    };
   }
-}
+};
 
 export const setQuery = (query) => {
   return {
     type: SET_QUERY,
     payload: query
-  }
-}
+  };
+};
 
 export const setListings = (updatedListings) => {
   return {
     type: SET_LISTINGS,
     payload: updatedListings
-  }
-}
+  };
+};
 
 export const fetchClaimedListings = (listingsId) =>{
-  const url = `/listing/claimed`;
+  const url = '/listing/claimed';
   const response = axios.post(url, {listingsId});
   return {
     type: FETCH_CLAIMED_LISTINGS,
     payload: response
-  }
-}
+  };
+};
--- client/src/reducers/UsersReducer.js
+++ client/src/reducers/UsersReducer.js
@@ -1,13 +1,13 @@
-import { FETCH_INTERESTED_USERS } from "../actions/UserActions";
+import { FETCH_INTERESTED_USERS } from '../actions/UserActions';
 // import { FETCH_USER } from "../actions/UserActions";
 
 
 export default (state = [], action) =>{
-  switch(action.type){
-    case FETCH_INTERESTED_USERS: return action.payload.data;
+  switch (action.type) {
+  case FETCH_INTERESTED_USERS: return action.payload.data;
   }
   return state;
-}
+};
 
 // module.exports.fetchUser = function(state = [], action) {
 //   switch(action.type){
