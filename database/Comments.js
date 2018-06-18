7418133581b35f3f187f151f3e6463ca2ffafa18
Heshie London <30602330+H-London@users.noreply.github.com>
2018-06-18 10:29:09 -0700

    Remove console.log statements

--- database/Comments.js
+++ database/Comments.js
@@ -15,7 +15,6 @@ let Comment = mongoose.model('Comment', commentSchema);
 exports.Comment = Comment;
 
 exports.saveComment = (comment) => {
-  console.log('This is your comment: ', comment);
   return new Promise((resolve, reject) => {
     let newComment = {};
     newComment.userId = comment.userId;
@@ -23,7 +22,6 @@ exports.saveComment = (comment) => {
     newComment.message = comment.text;
     
     let CommentToStore = new Comment(newComment);
-    console.log('This is comment to store: ', CommentToStore);
     
     CommentToStore.save(function(err) {
       Listing.Listing.findById(comment.listingId).exec((err, data) => {
@@ -31,7 +29,6 @@ exports.saveComment = (comment) => {
         data.save(err => err ? console.error(err) : undefined);
       })
       if (err) { reject(err); }
-      console.log('comment saved in db', CommentToStore)
       resolve(CommentToStore);
     });
   });
